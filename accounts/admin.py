5b4259430eca48398a4d1b470b8da0c0320eda3c
tdyushaxa <smaxmudov015@gmail.com>
2022-11-09 13:42:39 +0500

    my_own blog

---
+++ accounts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.forms import CostumUserCreationForm
+from accounts.models import CostumUser
+
+
+
+class CostumUserAdmin(UserAdmin):
+   
+    list_display = ['username', 'last_name', 'first_name', 'email', 'age', 'is_staff','male']
+    add_fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age',)}),
+                                           )
+    fieldsets = UserAdmin.fieldsets + ((None,{'fields': ['age',]}),
+                                       )
+
+admin.site.register(CostumUser,CostumUserAdmin)
